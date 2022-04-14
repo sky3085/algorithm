0dd78494af80b828285c09cfee6bddaf6c55fab3
yeonghwan Moon <skysky3085@daum.net>
2022-04-14 17:01:40 +0900

    4월 14일 Lv1 알고리즘 푸쉬

---
+++ Java/Lv1AddMethod.java
@@ -0,0 +1,29 @@
+package Algorithm_moon.Java;
+
+/*
+두개의 정수를 입력받아 그 사이의 모든 정수의 합을 구하시오.
+https://programmers.co.kr/learn/courses/30/lessons/12912?language=java
+*/
+
+public class Lv1AddMethod {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        long answer = solution(a,b);
+        System.out.println(answer);
+    }
+
+    static long solution(int a, int b) {
+        long answer = 0;
+        if(a<=b){
+            for(int i =a;i<=b;i++){
+                answer+=i;
+            }
+        }else{
+            for(int i =b;i<=a;i++){
+                answer+=i;
+            }
+        }
+        return answer;
+    }
+}
