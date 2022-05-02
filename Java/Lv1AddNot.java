2e8ca8a42d6a9985121b3e0855b4f7762e04bc5c
yeonghwan Moon <skysky3085@daum.net>
2022-05-02 21:45:46 +0900

    크롤링 과제 + Lv1파이썬으로 푼 문제 -> 자바

---
+++ Java/Lv1AddNot.java
@@ -0,0 +1,16 @@
+public class Lv1AddNot {
+    public static void main(String[] args) {
+        int[] numbers={1,2,3,4,5};
+        System.out.println(solution(numbers));
+
+    }
+    public static int solution(int[] numbers) {
+        int answer = 45;
+        int sum=0;
+        for(int i=0; i< numbers.length; i++){
+            sum+=numbers[i];
+        }
+        answer-=sum;
+        return answer;
+    }
+}
