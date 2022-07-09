dc3c571d54519d54b3c52375d0c684e4be0b3475
yeonghwan Moon <skysky3085@daum.net>
2022-07-09 22:57:49 +0900

    Lv1 프로그래머스 문제 풀이

---
+++ Java/Lv1Calc.java
@@ -0,0 +1,23 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/82612
+ */
+public class Lv1Calc {
+    public static long solution(int price, int money, int count) {
+
+        long total = 0;
+        for(int i=1; i<=count; i++){
+            total += (price*i);
+        }
+        if(total>money){
+            return total-money;
+        }else{
+            return 0;
+        }
+        
+    }
+    public static void main(String[] args) {
+        long a = solution(3, 20, 4);
+        System.out.println(a);
+    }
+    
+}
