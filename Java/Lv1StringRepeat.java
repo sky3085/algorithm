602a31d7db1913f78057b5370ce1c41d1a5239b7
yeonghwan Moon <skysky3085@daum.net>
2022-07-13 22:40:21 +0900

    프로그래머스 LV1 풀이

---
+++ Java/Lv1StringRepeat.java
@@ -0,0 +1,24 @@
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12922
+ */
+public class Lv1StringRepeat {
+    public static String solution(int n) {
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            if(i%2==0){
+                answer = answer.concat("수");
+            }else{
+                answer = answer.concat("박");
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String answer = solution(3);
+        System.out.println(answer);
+    }
+    
+}
