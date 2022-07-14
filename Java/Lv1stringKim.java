80de1b4cfca9e63fdd79bd7081b74d35730876ac
yeonghwan Moon <skysky3085@daum.net>
2022-07-14 22:20:38 +0900

    프로그래머스 LV1 풀이

---
+++ Java/Lv1stringKim.java
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+public class Lv1stringKim {
+    public static String solution(String[] seoul) {
+        String answer = "";
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 "+i+"에 있다";
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        String[] seoul = {"java", "Kim"};
+        String answer = solution(seoul);
+        System.out.println(answer);
+    }
+}
