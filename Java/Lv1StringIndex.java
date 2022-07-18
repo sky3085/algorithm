84bb82edb52f45f6fbdc3bc02164da86d475e5e0
yeonghwan Moon <skysky3085@daum.net>
2022-07-18 23:37:09 +0900

    프로그래머스 LV1 풀이

---
+++ Java/Lv1StringIndex.java
@@ -0,0 +1,28 @@
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+public class Lv1StringIndex {
+    public static String solution(String s) {
+        String answer = "";
+        int indexSize = s.length();
+        if(indexSize%2==0){
+            answer = s.substring(indexSize/2-1, indexSize/2+1);
+        }else{
+            answer = s.substring(indexSize/2, indexSize/2+1);
+        }
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+        String a = solution("abcdef");
+        String b = solution("abcde");
+        System.out.println(a);
+        System.out.println(b);
+    }
+    //다른 사람 풀이
+
+    String getMiddle(String word){
+
+        return word != null ? word.substring((word.length()-1)/2,(word.length()+2)/2) : "";
+      }
+}
